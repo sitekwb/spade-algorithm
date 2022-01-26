ba6bcbac4266c06b73879f1fb8de91c51817ff08
Wojciech Sitek <s.wojciech9@gmail.com>
2022-01-26 17:22:11 +0100

    Remove cmake-build-debug

--- cmake-build-debug/CMakeFiles/spade_algorithm.dir/src/IdList.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/spade_algorithm.dir/src/IdList.cpp.o: \
-  /Users/wojciechsitek/Documents/PW/MED/spade-algorithm/src/IdList.cpp \
-  /Users/wojciechsitek/Documents/PW/MED/spade-algorithm/src/../include/IdList.h
--- cmake-build-debug/CMakeFiles/spade_algorithm.dir/src/Subset.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/spade_algorithm.dir/src/Subset.cpp.o: \
-  /Users/wojciechsitek/Documents/PW/MED/spade-algorithm/src/Subset.cpp \
-  /Users/wojciechsitek/Documents/PW/MED/spade-algorithm/src/../include/Subset.h
